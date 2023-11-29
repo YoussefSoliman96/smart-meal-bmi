fbbb0574c2573172c7b8d34adf9ee9ee06844544
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-11-29 13:59:06 +0200

    Add BMI and Status to BMI page

--- app/bmi/page.tsx
+++ app/bmi/page.tsx
@@ -1,11 +1,43 @@
 "use client";
+import { Gender } from "@prisma/client";
 import { Button, Flex, Heading, Select, TextField } from "@radix-ui/themes";
-import Link from "next/link";
-import React from "react";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+
+interface BMI {
+  height: number;
+  weight: number;
+  age: number;
+  gender: Gender;
+}
 
 const page = () => {
+  const { register, handleSubmit, control } = useForm<BMI>();
+
+  const [bmi, setBmi] = useState(0);
+  const [status, setStatus] = useState("");
+
+  const calculateBMI = (height: number, weight: number) => {
+    const heightMeter = height / 100;
+    const heightSQR = heightMeter * heightMeter;
+    const bmi = parseFloat((weight / heightSQR).toFixed(2));
+    setBmi(bmi);
+    if (bmi < 18.5) {
+      setStatus("Underweight");
+    } else if (bmi < 24.9) {
+      setStatus("Healthy");
+    } else if (bmi < 29.9) {
+      setStatus("Overweight");
+    } else {
+      setStatus("Obese");
+    }
+  };
+
   return (
-    <div className="max-w-xl">
+    <form
+      className="max-w-xl"
+      onSubmit={handleSubmit((data) => calculateBMI(data.height, data.weight))}
+    >
       <Flex direction="column" gap="3" mb="3" style={{ maxWidth: 300 }}>
         <Flex direction="column" gap="3" mb="3">
           <Flex justify="between">
@@ -15,6 +47,7 @@ const page = () => {
             size="3"
             placeholder="Height in CM"
             defaultValue=""
+            {...register("height")}
           />
         </Flex>
         <Flex direction="column" gap="3" mb="3">
@@ -25,27 +58,44 @@ const page = () => {
             size="3"
             placeholder="Weight in KGs"
             defaultValue=""
+            {...register("weight")}
           />
         </Flex>
         <Flex direction="column" gap="3" mb="3">
           <Flex justify="between">
             <Heading>Age</Heading>
           </Flex>
-          <TextField.Input size="3" placeholder="Age" defaultValue="" />
+          <TextField.Input
+            size="3"
+            placeholder="Age"
+            defaultValue=""
+            {...register("age")}
+          />
         </Flex>
-        <Select.Root size="3">
-          <Select.Trigger placeholder="Gender" />
-          <Select.Content>
-            <Select.Group>
-              <Select.Label>Gender</Select.Label>
-              <Select.Item value="male">Male</Select.Item>
-              <Select.Item value="female">Female</Select.Item>
-            </Select.Group>
-          </Select.Content>
-        </Select.Root>
+        <Controller
+          name="gender"
+          control={control}
+          render={({ field }) => (
+            <Select.Root size="3" onValueChange={field.onChange}>
+              <Select.Trigger placeholder="Gender" />
+              <Select.Content>
+                <Select.Group>
+                  <Select.Label>Gender</Select.Label>
+                  <Select.Item value="male">Male</Select.Item>
+                  <Select.Item value="female">Female</Select.Item>
+                </Select.Group>
+              </Select.Content>
+            </Select.Root>
+          )}
+        ></Controller>
       </Flex>
       <Button size="3">Calculate BMI</Button>
-    </div>
+      {bmi > 0 ? (
+        <Heading my="5">
+          BMI: {bmi}, you are {status}!
+        </Heading>
+      ) : null}
+    </form>
   );
 };
 
---
+++ prisma/migrations/20231129110138_create_user/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `age` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `height` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `name` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `weight` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `User` ADD COLUMN `age` INTEGER NOT NULL,
+    ADD COLUMN `email` VARCHAR(191) NOT NULL,
+    ADD COLUMN `gender` ENUM('MALE', 'Female') NULL,
+    ADD COLUMN `height` INTEGER NOT NULL,
+    ADD COLUMN `name` VARCHAR(191) NOT NULL,
+    ADD COLUMN `weight` INTEGER NOT NULL,
+    MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `User_email_key` ON `User`(`email`);
